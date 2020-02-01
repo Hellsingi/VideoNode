6cd4068854a58af2ce8290ab35a986b7ae679299
Hellsingi <f.c.realmadrid@mail.ru>
2020-02-01 23:03:27 +0300

    Create rest api-server

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
     if (req.method === 'GET') {
         res.writeHead(200, {
-            'Content-Type': 'text/html'
+            'Content-Type': 'text/html; charset=utf-8'
         });
-        res.end(`
-            <h1>From</h1>
-            <form method="post" action="/">
-                <input name="title" type="text">
-                <button type="submit">Send</button>
-            </form>
-        `);
+
+        if (req.url === '/') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'index.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err
+                    }
+
+                    res.end(content)
+                }
+            );
+        } else if (req.url === '/about') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'about.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) throw err;
+
+                    res.end(content);
+                }
+            );
+        } else if (req.url === '/api/users') {
+            res.writeHead(200,
+                {
+                    'Content-Type': 'text/json'
+                })
+
+            const users = [
+                { name: 'Ilea', age: 28 },
+                { name: 'Iryan', age: 23 }
+            ]
+
+            res.end(JSON.stringify(users));
+        }
+
     } else if (req.method === 'POST') {
         const body = [];
         res.writeHead(200, {
@@ -23,7 +55,6 @@ const server = http.createServer((req, res) => {
         });
 
         req.on('end', () => {
-            // console.log(body.toString());
             const message = body.toString().split('=')[1];
 
             res.end(`

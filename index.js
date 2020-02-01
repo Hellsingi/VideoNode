fda0420be9493d4f27f817995beb91c5f7a6a5ae
Hellsingi <f.c.realmadrid@mail.ru>
2020-02-01 23:33:59 +0300

    Basic setup for express

--- index.js
+++ index.js
@@ -1,69 +1,9 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html; charset=utf-8'
-        });
+const app = express();
 
-        if (req.url === '/') {
-            fs.readFile(
-                path.join(__dirname, 'views', 'index.html'),
-                'utf-8',
-                (err, content) => {
-                    if (err) {
-                        throw err
-                    }
+const PORT = process.env.PORT || 3000;
 
-                    res.end(content)
-                }
-            );
-        } else if (req.url === '/about') {
-            fs.readFile(
-                path.join(__dirname, 'views', 'about.html'),
-                'utf-8',
-                (err, content) => {
-                    if (err) throw err;
-
-                    res.end(content);
-                }
-            );
-        } else if (req.url === '/api/users') {
-            res.writeHead(200,
-                {
-                    'Content-Type': 'text/json'
-                })
-
-            const users = [
-                { name: 'Ilea', age: 28 },
-                { name: 'Iryan', age: 23 }
-            ]
-
-            res.end(JSON.stringify(users));
-        }
-
-    } else if (req.method === 'POST') {
-        const body = [];
-        res.writeHead(200, {
-            'Content-Type': 'text/html; charset=utf-8;'
-        })
-
-        req.on('data', data => {
-            body.push(Buffer.from(data))
-        });
-
-        req.on('end', () => {
-            const message = body.toString().split('=')[1];
-
-            res.end(`
-            <h1>Ваше message: ${message} </h1>
-            `);
-        });
-    }
-});
-
-server.listen(3000, () => {
-    console.log('Server is running ...')
-});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})

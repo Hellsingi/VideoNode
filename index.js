4a1ef5bc1ca62ec2117d60ba0b0f96dab0b9b4b6
Hellsingi <f.c.realmadrid@mail.ru>
2020-02-02 00:51:23 +0300

    Add navigation

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'pages');
 
-
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index');

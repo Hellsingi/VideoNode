19c7b51259c01e006d92fcb0a2e9444a5547cdf6
Hellsingi <f.c.realmadrid@mail.ru>
2020-02-02 21:07:50 +0300

    Asynchronous Request Processing

--- models/card.js
+++ models/card.js
@@ -28,10 +28,37 @@ class Card {
 
         return new Promise((resolve, reject) => {
             fs.writeFile(p, JSON.stringify(card), err => {
-                if(err) {
+                if (err) {
                     reject(err);
                 } else {
                     resolve();
+                }
+            })
+        });
+    }
+
+    static async remove(id) {
+        const card = await Card.fetch();
+
+        const idx = card.courses.findIndex(c => c.id === id);
+        const course = card.courses[idx];
+
+        if (course.count === 1) {
+            // delete course
+            card.courses = card.courses.filter(c => c.id !== id);
+        } else {
+            // change count
+            card.courses[idx].count--;
+        }
+
+        card.price -= course.price;
+
+        return new Promise((resolve, reject) => {
+            fs.writeFile(p, JSON.stringify(card), err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(card);
                 }
             })
         });

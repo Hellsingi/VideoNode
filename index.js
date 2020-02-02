cb90a4f6d01078e693382253691887e88a59314f
Hellsingi <f.c.realmadrid@mail.ru>
2020-02-02 14:53:43 +0300

    Forms processing

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.set('view engine', 'hbs');
 app.set('views', 'pages');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use('/', homeRoutes);
 app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
--- routes/add.js
+++ routes/add.js
@@ -7,5 +7,10 @@ router.get('/', (req, res) => {
         isAdd: true
     });
 });
+router.post('/', (req, res) => {
+    console.log(req.body);
+
+    res.redirect('/courses');
+})
 
 module.exports = router;

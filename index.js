00955b48d318704afd5c99b61869b53a2739f93e
Hellsingi <f.c.realmadrid@mail.ru>
2020-02-02 14:40:34 +0300

    Add router

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const homeRoutes = require('./routes/home');
+const addRoutes = require('./routes/add');
+const coursesRoutes = require('./routes/courses');
+
 
 const app = express();
 
@@ -14,28 +18,9 @@ app.set('view engine', 'hbs');
 app.set('views', 'pages');
 
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Main pages',
-        isHome: true
-    });
-});
-
-app.get('/add', (req, res) => {
-    res.render('add', {
-        title: 'Add course',
-        isAdd: true
-    });
-});
-
-app.get('/courses', (req, res) => {
-    res.render('courses', {
-        title: 'Courses',
-        isCourses: true
-    });
-});
-
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
 
 
 const PORT = process.env.PORT || 3000;

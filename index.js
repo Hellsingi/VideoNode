151cd3632352e1976680f852cc8a0f5555a22f16
Hellsingi <f.c.realmadrid@mail.ru>
2020-02-09 00:34:11 +0300

    Route Protection

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ const MONGODB_URI = 'mongodb://Hellsingi:bcgfybz@cluster0-shard-00-00-8rsx0.mong
 
 const app = express();
 const hbs = exphbs.create({
-  defaultLayout: 'main',
-  extname: 'hbs'
+    defaultLayout: 'main',
+    extname: 'hbs'
 });
 const store = new MongoStore({
-  collection: 'sessions',
-  uri: MONGODB_URI
+    collection: 'sessions',
+    uri: MONGODB_URI
 });
 
 app.engine('hbs', hbs.engine);
@@ -30,12 +30,12 @@ app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(session({
-  secret: 'some secret value',
-  resave: false,
-  saveUninitialized: false,
-  store
+    secret: 'some secret value',
+    resave: false,
+    saveUninitialized: false,
+    store
 }));
 app.use(varMiddleware);
 
@@ -49,17 +49,17 @@ app.use('/auth', authRoutes);
 const PORT = process.env.PORT || 3000;
 
 async function start() {
-  try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useFindAndModify: false
-    });
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`)
-    });
-  } catch (e) {
-    console.log(e)
-  }
+    try {
+        await mongoose.connect(MONGODB_URI, {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        });
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        });
+    } catch (e) {
+        console.log(e)
+    }
 };
 
 start();

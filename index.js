e009432bf6623533f6a2c737a853f87ff2c381d7
Hellsingi <f.c.realmadrid@mail.ru>
2020-02-08 16:45:05 +0300

    Add users

--- index.js
+++ index.js
@@ -6,27 +6,38 @@ const homeRoutes = require('./routes/home');
 const cardRoutes = require('./routes/card');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
+const User = require('./models/user');
 
-const app = express()
+const app = express();
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
-})
+});
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({ extended: true }))
+app.use(async (req, res, next) => {
+    try {
+        const user = await User.findById('5e3eb7a341d95527f05c12d6');
+        req.user = user;
+        next();
+    } catch (e) {
+        console.error(e);
+    }
+});
 
-app.use('/', homeRoutes)
-app.use('/add', addRoutes)
-app.use('/courses', coursesRoutes)
-app.use('/card', cardRoutes)
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3000
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
+app.use('/card', cardRoutes);
+
+const PORT = process.env.PORT || 3000;
 
 async function start() {
     try {
@@ -36,13 +47,22 @@ async function start() {
             useNewUrlParser: true,
             useFindAndModify: false
         });
+        const candidate = await User.findOne();
+        if (!candidate) {
+            const user = new User({
+                email: 'f.c.realmadrid@mail.ru',
+                name: 'Ilya',
+                cart: { items: [] }
+            });
+            await user.save();
+        }
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
         });
     } catch (e) {
         console.log(e)
     }
-}
+};
 
 start();
 

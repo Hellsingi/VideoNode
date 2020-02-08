7777ac2448c12fa528287faac38d9e33e46b43f8
Hellsingi <f.c.realmadrid@mail.ru>
2020-02-08 23:44:50 +0300

    Autentification

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 const homeRoutes = require('./routes/home');
 const cardRoutes = require('./routes/card');
 const addRoutes = require('./routes/add');
-const ordersRoutes =  require('./routes/orders');
+const ordersRoutes = require('./routes/orders');
 const coursesRoutes = require('./routes/courses');
 const authRoutes = require('./routes/auth');
 const User = require('./models/user');
+const varMiddleware = require('./middleware/variables');
 
 const app = express();
 
@@ -33,6 +35,12 @@ app.use(async (req, res, next) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'some secret value',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(varMiddleware);
 
 app.use('/', homeRoutes);
 app.use('/add', addRoutes);

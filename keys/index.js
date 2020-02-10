0538ed560f1307521adcd9658c7eabdf8b92890c
Hellsingi <f.c.realmadrid@mail.ru>
2020-02-10 23:48:24 +0300

    Basic config

---
+++ keys/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB_URI: 'mongodb://Hellsingi:bcgfybz@cluster0-shard-00-00-8rsx0.mongodb.net:27017,cluster0-shard-00-01-8rsx0.mongodb.net:27017,cluster0-shard-00-02-8rsx0.mongodb.net:27017/shop?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+    SESSION_SECRET: 'some secret value',
+};
